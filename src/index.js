2317768dc48e1d7d86785bac8a521865a6963009
Mateus Costa <mateus.costa@roit.com.br>
2021-12-26 16:25:11 -0300

    feat: criando saque na conta

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ function verifyExistsAccountCPF(resquest, response, next) {
     return next()
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
+
 /**
  * cpf - string
  * name - string
@@ -69,6 +81,27 @@ app.post('/deposit', verifyExistsAccountCPF, (resquest, response) => {
         amount,
         create_at: new Date(),
         type: 'credit'
+    }
+
+    customer.statement.push(statementOperations)
+
+    return response.status(201).send()
+})
+
+app.post('/withdraw', verifyExistsAccountCPF, (resquest, response) => {
+    const { amount } = resquest.body
+    const { customer } = resquest
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({ error: 'Insufficient funds!'})
+    }
+
+    const statementOperations = {
+        amount,
+        create_at: new Date(),
+        type: 'debit'
     }
 
     customer.statement.push(statementOperations)
